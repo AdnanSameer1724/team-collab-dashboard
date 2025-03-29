0f4aa340ecab4be427dc3193a886f0a7db457495
adnan50 <adnan@livekeeping.com>
2025-03-29 17:46:02 +0530

    Implementing tasks and its related functionalities

--- server/models/Task.js
+++ server/models/Task.js
@@ -4,7 +4,10 @@ const taskSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: String,
     status: { type: String, enum: ['todo', 'in-progress', 'done'],  default: 'todo'},
-    priority: { type: String, enum: ['low', 'medium', 'high']},
-    dueDate: Date,
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
-});
+    priority: { type: String, enum: ['low', 'medium', 'high'], default: 'medium'},
+    dueDate: { type: Date },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Task', taskSchema);
